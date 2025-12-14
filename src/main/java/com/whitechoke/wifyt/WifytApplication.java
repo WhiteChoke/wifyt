9c53c33f9c5480ac65061346bfed4f327ef6643e
WhiteChoke <denisduchenkoo@gmail.com>
2025-12-14 22:47:40 +0900

    init commit

---
+++ src/main/java/com/whitechoke/wifyt/WifytApplication.java
@@ -0,0 +1,13 @@
+package com.whitechoke.wifyt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifytApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifytApplication.class, args);
+	}
+
+}
