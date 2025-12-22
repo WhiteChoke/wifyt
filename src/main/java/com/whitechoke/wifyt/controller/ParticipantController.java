9de260d267864d44c64131edb1ac3c6798dffd57
WhiteChoke <denisduchenkoo@gmail.com>
2025-12-22 21:26:42 +0900

    Created controller for participant

---
+++ src/main/java/com/whitechoke/wifyt/controller/ParticipantController.java
@@ -0,0 +1,65 @@
+package com.whitechoke.wifyt.controller;
+
+import com.whitechoke.wifyt.dto.Participant;
+import com.whitechoke.wifyt.service.ParticipantService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/participant")
+@AllArgsConstructor
+public class ParticipantController {
+
+    private final ParticipantService service;
+
+    @PostMapping
+    public ResponseEntity<Participant> createParticipant(@RequestBody Participant participantToCreate) {
+
+        var createdParticipant = service.createParticipant(participantToCreate);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(createdParticipant);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Participant> getParticipantById(@PathVariable UUID id) {
+
+        var participant = service.getParticipantById(id);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(participant);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Participant> updateParticipantById(@PathVariable UUID id,
+                                                          @RequestBody Participant participant) {
+
+        var updatedParticipant = service.updateParticipant(id, participant);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(updatedParticipant);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Void> deleteParticipantById(@PathVariable UUID id) {
+
+        service.deleteParticipantById(id);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .build();
+    }
+}
