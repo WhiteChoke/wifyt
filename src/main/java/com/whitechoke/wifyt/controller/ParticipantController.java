63dd346cf25886e18bae55a7281819c3bd50a442
WhiteChoke <denisduchenkoo@gmail.com>
2025-12-23 11:31:20 +0900

    fix bug

--- src/main/java/com/whitechoke/wifyt/controller/ParticipantController.java
+++ src/main/java/com/whitechoke/wifyt/controller/ParticipantController.java
@@ -5,6 +5,7 @@ import com.whitechoke.wifyt.service.ParticipantService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,7 +52,7 @@ public class ParticipantController {
                 .body(updatedParticipant);
     }
 
-    @GetMapping("{id}")
+    @DeleteMapping("{id}")
     public ResponseEntity<Void> deleteParticipantById(@PathVariable Long id) {
 
         service.deleteParticipantById(id);
