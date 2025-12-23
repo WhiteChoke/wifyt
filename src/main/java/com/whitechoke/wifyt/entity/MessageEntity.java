e8c077317671aafd884199ae88223716de49e394
WhiteChoke <denisduchenkoo@gmail.com>
2025-12-23 16:06:41 +0900

    Updated message entity

--- src/main/java/com/whitechoke/wifyt/entity/MessageEntity.java
+++ src/main/java/com/whitechoke/wifyt/entity/MessageEntity.java
@@ -3,6 +3,7 @@ package com.whitechoke.wifyt.entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -10,13 +11,14 @@ import java.time.Instant;
 
 @Data
 @Document(collation = "messages")
+@AllArgsConstructor
 public class MessageEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long senderId;
-    private Long chatId;
+    private ParticipantEntity sender;
+    private ChatEntity chat;
     // will soon be replaced by
     private String message;
     private Instant created_at;
