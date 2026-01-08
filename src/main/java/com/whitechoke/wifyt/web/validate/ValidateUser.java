0f7fbdf331c1c5be6bdac0d26ade6c63d9d14a8e
WhiteChoke <denisduchenkoo@gamil.com>
2026-01-09 01:24:06 +0900

    created validation class

--- src/main/java/com/whitechoke/wifyt/web/validate/ValidateUser.java
+++ src/main/java/com/whitechoke/wifyt/web/validate/ValidateUser.java
@@ -1,4 +1,23 @@
 package com.whitechoke.wifyt.web.validate;
 
+import com.whitechoke.wifyt.dto.RequestToCreateUser;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ValidateUser {
+
+    public void validateUser(RequestToCreateUser user) {
+        if (user.email() == null) {
+            throw new IllegalArgumentException("Email cannot be empty");
+        }
+
+        if (user.username() == null) {
+            throw new IllegalArgumentException("Username cannot be empty");
+        }
+
+        if (user.password() == null) {
+            throw new IllegalArgumentException("Password cannot be empty");
+        }
+
+    }
 }
