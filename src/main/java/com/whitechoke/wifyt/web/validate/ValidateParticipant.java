31e802d77caf9e30dd05920304d80148b62c8733
WhiteChoke <denisduchenkoo@gmail.com>
2026-01-09 17:27:41 +0900

    created validation class for participant

--- src/main/java/com/whitechoke/wifyt/web/validate/ValidateParticipant.java
+++ src/main/java/com/whitechoke/wifyt/web/validate/ValidateParticipant.java
@@ -1,4 +1,20 @@
 package com.whitechoke.wifyt.web.validate;
 
+import com.whitechoke.wifyt.dto.participant.ParticipantRequest;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ValidateParticipant {
+
+    public void validate(ParticipantRequest request) {
+        if (request.userId() == null) {
+            throw new IllegalArgumentException("User id cannot be empty");
+        }
+        if (request.role() == null) {
+            throw new IllegalArgumentException("Participant role cannot be empty");
+        }
+        if (request.chatId() == null) {
+            throw new IllegalArgumentException("Chat id cannot be empty");
+        }
+    }
 }
