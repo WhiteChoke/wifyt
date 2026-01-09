c698657d4abd8f349e0ed1966bd67e2c2525ad15
WhiteChoke <denisduchenkoo@gmail.com>
2026-01-09 19:31:20 +0900

    created message request dto

---
+++ src/main/java/com/whitechoke/wifyt/dto/message/MessageRequest.java
@@ -0,0 +1,13 @@
+package com.whitechoke.wifyt.dto.message;
+
+import jakarta.validation.constraints.NotNull;
+
+public record MessageRequest(
+        @NotNull
+        Long senderId,
+        @NotNull
+        Long chatId,
+        @NotNull
+        String message
+) {
+}
