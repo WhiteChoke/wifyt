04651e44e6d88eef645715de85049bcc145461ae
WhiteChoke <denisduchenkoo@gmail.com>
2026-01-09 17:27:02 +0900

    created participant request dto

---
+++ src/main/java/com/whitechoke/wifyt/dto/participant/ParticipantRequest.java
@@ -0,0 +1,14 @@
+package com.whitechoke.wifyt.dto.participant;
+
+import com.whitechoke.wifyt.enums.UserRoles;
+import jakarta.validation.constraints.NotNull;
+
+public record ParticipantRequest (
+        @NotNull
+        Long userId,
+        @NotNull
+        UserRoles role,
+        @NotNull
+        Long chatId
+){
+}
---
+++ src/main/java/com/whitechoke/wifyt/web/validate/ValidateParticipant.java
@@ -0,0 +1,4 @@
+package com.whitechoke.wifyt.web.validate;
+
+public class ValidateParticipant {
+}
