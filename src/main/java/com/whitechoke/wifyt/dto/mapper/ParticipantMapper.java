4835049eddfa7f4b7558801e9090a7d9677a764e
WhiteChoke <denisduchenkoo@gmail.com>
2026-01-09 17:41:29 +0900

    overloaded toEntity method

--- src/main/java/com/whitechoke/wifyt/dto/mapper/ParticipantMapper.java
+++ src/main/java/com/whitechoke/wifyt/dto/mapper/ParticipantMapper.java
@@ -1,6 +1,7 @@
 package com.whitechoke.wifyt.dto.mapper;
 
 import com.whitechoke.wifyt.dto.Participant;
+import com.whitechoke.wifyt.dto.participant.ParticipantRequest;
 import com.whitechoke.wifyt.entity.ParticipantEntity;
 import com.whitechoke.wifyt.repository.ChatRepository;
 import com.whitechoke.wifyt.repository.UserRepository;
@@ -25,17 +26,15 @@ public class ParticipantMapper {
     }
 
     public ParticipantEntity toEntity(Participant dto) {
-
-        var user = userRepository.findById(dto.userId())
-                .orElseThrow(() -> new EntityNotFoundException("Not found user by id: " + dto.userId()));
-        var chat = chatRepository.findById(dto.chatId())
-                .orElseThrow(() -> new EntityNotFoundException("Not found chat by id: " + dto.userId()));
-
         return new ParticipantEntity(
                 dto.id(),
-                user,
-                dto.role(),
-                chat
+                dto.role()
         );
+    }
+
+    public ParticipantEntity toEntity(ParticipantRequest request) {
+        var entity = new ParticipantEntity();
+        entity.setRole(request.role());
+        return entity;
     }
 }
